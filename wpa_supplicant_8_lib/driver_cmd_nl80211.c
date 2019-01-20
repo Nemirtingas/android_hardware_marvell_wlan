228a55c01e9e3bc8fcde7de48fc5b27d954dd588
Nemirtingas <nanaki89@hotmail.fr>
2019-01-20 17:03:21 +0100

    Updated for cm-12.1

--- wpa_supplicant_8_lib/driver_cmd_nl80211.c
+++ wpa_supplicant_8_lib/driver_cmd_nl80211.c
@@ -1,25 +1,21 @@
 /*
- * Driver interaction with extended Linux CFG8021
+ * Copyright (C) 2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Alternatively, this software may be distributed under the terms of BSD
- * license.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#include "includes.h"
-#include <sys/types.h>
-#include <fcntl.h>
-#include <net/if.h>
-#include <netlink/msg.h>
-#include <netlink/genl/genl.h>
-
-#include "common.h"
-#include "linux_ioctl.h"
-#include "driver_nl80211.h"
+#include "hardware_legacy/driver_nl80211.h"
 #include "wpa_supplicant_i.h"
 #include "config.h"
 #ifdef ANDROID
@@ -29,7 +25,7 @@
 #define WPA_PS_ENABLED		0
 #define WPA_PS_DISABLED		1
 
-#define os_strncpy strncpy
+//EVENT_CHANNEL_LIST_CHANGED,
 
 /* Return type for setBand*/
 enum {
@@ -38,11 +34,10 @@ enum {
 };
 
 typedef struct android_wifi_priv_cmd {
-    union
-    {
-        char* pbuf;
-        uint8_t buf[8];
-    } priv_data;
+    union {
+	    char *pbuf;
+        char buf[8];
+    } datas;
 	int used_len;
 	int total_len;
 } android_wifi_priv_cmd;
@@ -55,6 +50,26 @@ static void wpa_driver_send_hang_msg(struct wpa_driver_nl80211_data *drv)
 	if (drv_errors > DRV_NUMBER_SEQUENTIAL_ERRORS) {
 		drv_errors = 0;
 		wpa_msg(drv->ctx, MSG_INFO, WPA_EVENT_DRIVER_STATE "HANGED");
+	}
+}
+
+static void wpa_driver_notify_country_change(void *ctx, char *cmd)
+{
+	if ((os_strncasecmp(cmd, "COUNTRY", 7) == 0) ||
+	    (os_strncasecmp(cmd, "SETBAND", 7) == 0)) {
+		union wpa_event_data event;
+		os_memset(&event, 0, sizeof(event));
+		event.channel_list_changed.initiator = REGDOM_SET_BY_USER;
+		if (os_strncasecmp(cmd, "COUNTRY", 7) == 0) {
+			event.channel_list_changed.type = REGDOM_TYPE_COUNTRY;
+			if (os_strlen(cmd) > 9) {
+				event.channel_list_changed.alpha2[0] = cmd[8];
+				event.channel_list_changed.alpha2[1] = cmd[9];
+			}
+		} else {
+			event.channel_list_changed.type = REGDOM_TYPE_UNKNOWN;
+		}
+		wpa_supplicant_event(ctx, EVENT_CHANNEL_LIST_CHANGED, &event);
 	}
 }
 
@@ -125,14 +140,13 @@ nla_put_failure:
 	return ret;
 }
 
-static int wpa_driver_set_backgroundscan_params(struct i802_bss *bss, const char *cmd)
+static int wpa_driver_set_backgroundscan_params(void *priv)
 {
+    return -1;
     /*
-	struct i802_bss *bss = priv;
-	struct wpa_driver_nl80211_data *drv = bss->drv;
+	struct wpa_driver_wext_data *drv = priv;
 	struct wpa_supplicant *wpa_s;
-	struct ifreq ifr;
-	android_wifi_priv_cmd priv_cmd;
+	struct iwreq iwr;
 	int ret = 0, i = 0, bp;
 	char buf[WEXT_PNO_MAX_COMMAND_SIZE];
 	struct wpa_ssid *ssid_conf;
@@ -158,9 +172,9 @@ static int wpa_driver_set_backgroundscan_params(struct i802_bss *bss, const char
 	buf[bp++] = WEXT_PNO_TLV_RESERVED;
 	while ((i < WEXT_PNO_AMOUNT) && (ssid_conf != NULL)) {
 		// Check that there is enough space needed for 1 more SSID, the other sections and null termination
-		if ((bp + WEXT_PNO_SSID_HEADER_SIZE + MAX_SSID_LEN + WEXT_PNO_NONSSID_SECTIONS_SIZE + 1) >= (int)sizeof(buf))
+		if ((bp + WEXT_PNO_SSID_HEADER_SIZE + IW_ESSID_MAX_SIZE + WEXT_PNO_NONSSID_SECTIONS_SIZE + 1) >= (int)sizeof(buf))
 			break;
-		if ((!ssid_conf->disabled) && (ssid_conf->ssid_len <= MAX_SSID_LEN)){
+		if ((!ssid_conf->disabled) && (ssid_conf->ssid_len <= IW_ESSID_MAX_SIZE)){
 			wpa_printf(MSG_DEBUG, "For PNO Scan: %s", ssid_conf->ssid);
 			buf[bp++] = WEXT_PNO_SSID_SECTION;
 			buf[bp++] = ssid_conf->ssid_len;
@@ -179,23 +193,23 @@ static int wpa_driver_set_backgroundscan_params(struct i802_bss *bss, const char
 	buf[bp++] = WEXT_PNO_MAX_REPEAT_SECTION;
 	os_snprintf(&buf[bp], WEXT_PNO_MAX_REPEAT_LENGTH + 1, "%x", WEXT_PNO_MAX_REPEAT);
 	bp += WEXT_PNO_MAX_REPEAT_LENGTH + 1;
-	memset(&ifr, 0, sizeof(ifr));
-	memset(&priv_cmd, 0, sizeof(priv_cmd));
-	os_strncpy(ifr.ifr_name, bss->ifname, IFNAMSIZ);
-	priv_cmd.buf = buf;
-	priv_cmd.used_len = bp;
-	priv_cmd.total_len = bp;
-	ifr.ifr_data = &priv_cmd;
-	ret = ioctl(drv->global->ioctl_sock, SIOCDEVPRIVATE + 1, &ifr);
+	os_memset(&iwr, 0, sizeof(iwr));
+	os_strncpy(iwr.ifr_name, drv->ifname, IFNAMSIZ);
+	iwr.u.data.pointer = buf;
+	iwr.u.data.length = bp;
+	ret = ioctl(drv->ioctl_sock, SIOCSIWPRIV, &iwr);
 	if (ret < 0) {
 		wpa_printf(MSG_ERROR, "ioctl[SIOCSIWPRIV] (pnosetup): %d", ret);
-		wpa_driver_send_hang_msg(drv);
+		drv->errors++;
+		if (drv->errors > DRV_NUMBER_SEQUENTIAL_ERRORS) {
+			drv->errors = 0;
+			wpa_msg(drv->ctx, MSG_INFO, WPA_EVENT_DRIVER_STATE "HANGED");
+		}
 	} else {
-		drv_errors = 0;
+		drv->errors = 0;
 	}
 	return ret;
-	*/
-	return -1;
+    */
 }
 
 int wpa_driver_nl80211_driver_cmd(void *priv, char *cmd, char *buf,
@@ -203,25 +217,27 @@ int wpa_driver_nl80211_driver_cmd(void *priv, char *cmd, char *buf,
 {
 	struct i802_bss *bss = priv;
 	struct wpa_driver_nl80211_data *drv = bss->drv;
+	struct wpa_driver_nl80211_data *driver;
 	struct ifreq ifr;
 	android_wifi_priv_cmd priv_cmd;
 	int ret = 0;
+
 	if (os_strcasecmp(cmd, "STOP") == 0) {
-		linux_set_iface_flags(drv->global->ioctl_sock, bss->ifname, 0);
+        linux_set_iface_flags(drv->global->ioctl_sock, bss->ifname, 0);
 		wpa_msg(drv->ctx, MSG_INFO, WPA_EVENT_DRIVER_STATE "STOPPED");
 	} else if (os_strcasecmp(cmd, "START") == 0) {
-		linux_set_iface_flags(drv->global->ioctl_sock, bss->ifname, 1);
+        linux_set_iface_flags(drv->global->ioctl_sock, bss->ifname, 1);
 		wpa_msg(drv->ctx, MSG_INFO, WPA_EVENT_DRIVER_STATE "STARTED");
 	} else if (os_strcasecmp(cmd, "MACADDR") == 0) {
-		u8 macaddr[ETH_ALEN] = {};
+        u8 macaddr[ETH_ALEN] = {};
 		ret = linux_get_ifhwaddr(drv->global->ioctl_sock, bss->ifname, macaddr);
 		if (!ret)
 			ret = os_snprintf(buf, buf_len,
 					  "Macaddr = " MACSTR "\n", MAC2STR(macaddr));
-	} else if (os_strcasecmp(cmd, "RELOAD") == 0) {
+    } else if (os_strcasecmp(cmd, "RELOAD") == 0) {
 		wpa_msg(drv->ctx, MSG_INFO, WPA_EVENT_DRIVER_STATE "HANGED");
 	} else if (os_strncasecmp(cmd, "POWERMODE ", 10) == 0) {
-		int state;
+        int state;
 		state = atoi(cmd + 10);
 		ret = wpa_driver_set_power_save(priv, state);
 		if (ret < 0)
@@ -229,7 +245,7 @@ int wpa_driver_nl80211_driver_cmd(void *priv, char *cmd, char *buf,
 		else
 			drv_errors = 0;
 	} else if (os_strncasecmp(cmd, "GETPOWER", 8) == 0) {
-		int state = -1;
+        int state = -1;
 		ret = wpa_driver_get_power_save(priv, &state);
 		if (!ret && (state != -1)) {
 			ret = os_snprintf(buf, buf_len, "POWERMODE = %d\n", state);
@@ -237,39 +253,40 @@ int wpa_driver_nl80211_driver_cmd(void *priv, char *cmd, char *buf,
 		} else {
 			wpa_driver_send_hang_msg(drv);
 		}
-	} else if (os_strncasecmp(cmd, "BGSCAN-START", 12) == 0) {
-        ret = wpa_driver_set_backgroundscan_params(bss, cmd);
-        if (ret < 0) {
-            wpa_printf(MSG_ERROR, "%s: failed to issue private command: BGSCAN-START\n", __func__);
-        }
+    } else if (os_strcasecmp(cmd, "BGSCAN-START") == 0) {
+        ret = wpa_driver_set_backgroundscan_params(priv);
+		if (ret < 0) {
+            wpa_printf(MSG_DEBUG, "%s: failed to issue private command: BGSCAN-START\n", __func__);
+		}
 	} else { /* Use private command */
-	    memcpy(buf, cmd, strlen(cmd)+1);
 		memset(&ifr, 0, sizeof(ifr));
 		memset(&priv_cmd, 0, sizeof(priv_cmd));
-		os_strncpy(ifr.ifr_name, bss->ifname, IFNAMSIZ);
-		priv_cmd.priv_data.pbuf = buf;
+		os_memcpy(buf, cmd, strlen(cmd) + 1);
+		os_strlcpy(ifr.ifr_name, bss->ifname, IFNAMSIZ);
+
+		priv_cmd.datas.pbuf = buf;
 		priv_cmd.used_len = buf_len;
 		priv_cmd.total_len = buf_len;
 		ifr.ifr_data = &priv_cmd;
-		if ((ret = ioctl(drv->global->ioctl_sock, SIOCDEVPRIVATE + 14, &ifr)) < 0) {
+
+		if ((ret = ioctl(drv->global->ioctl_sock, SIOCDEVPRIVATE + 0xE, &ifr)) < 0) {
 			wpa_printf(MSG_ERROR, "%s: failed to issue private commands\n", __func__);
-			wpa_printf(MSG_ERROR, "the cmd is %s\n", cmd);
-			wpa_printf(MSG_ERROR, "the error is [%d]  [%s] \n", errno, strerror(errno));
-			wpa_driver_send_hang_msg(drv);
+            wpa_printf(MSG_ERROR, "the cmd is %s\n", cmd);
+            wpa_printf(MSG_ERROR, "the error is [%d]  [%s] \n", errno, strerror(errno));
 		} else {
 			drv_errors = 0;
-			ret = 0;
 			if ((os_strcasecmp(cmd, "LINKSPEED") == 0) ||
 			    (os_strcasecmp(cmd, "RSSI") == 0) ||
 			    (os_strcasecmp(cmd, "GETBAND") == 0) ||
 			    (os_strncasecmp(cmd, "WLS_BATCHING", 12) == 0) )
 				ret = strlen(buf);
-			else if (os_strcasecmp(cmd, "COUNTRY") == 0 ||
-                     os_strcasecmp(cmd, "SETBAND") == 0)
-				wpa_supplicant_event(drv->ctx,
-					EVENT_CHANNEL_LIST_CHANGED, NULL);
-			wpa_printf(MSG_DEBUG, "%s %s len = %d, %d", __func__, buf, ret, strlen(buf));
+			else
+            {
+                wpa_driver_notify_country_change(drv->ctx, cmd);
+			    ret = 0;
+            }
 		}
+        wpa_printf(MSG_DEBUG, "%s %s len = %d, %d", __func__, buf, ret, strlen(buf));
 	}
 	return ret;
 }
